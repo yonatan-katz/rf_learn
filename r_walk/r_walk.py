85714d69efc97b38546b341d55adb630471be990
Yonatan Katz <yonic2006@gmail.com>
2017-05-30 21:41:31 +0300

    Add TD(lambda) control algo

--- r_walk/r_walk.py
+++ r_walk/r_walk.py
@@ -41,17 +41,17 @@ def flat_dict(d):
     for k in sorted(d.keys()):
         v.append(d[k])
     return v
-
-def td1(alpha=0.01,discount=1):
-    def get_mse(V,v):
+    
+def get_mse(V,v):
         e = 0
         for k in V.keys():
             e += ((V[k] - v[k])**2)
         return np.sqrt(e/len(V.keys()))
-            
+
+def td1(alpha=0.05,discount=1):            
     R = RWalk()    
     V = {'A':0.0,'B':0.0,'C':0.0,'D':0.0,'E':0.0}
-    episode = 1000
+    episode = 100
     E = []
     while episode > 0:
         S = R.start_new_episode()  
@@ -68,6 +68,67 @@ def td1(alpha=0.01,discount=1):
         episode -= 1       
     
     return E,V
+    
+def td_lambda(lamda,alpha,discount=1):
+    R = RWalk()
+    V = {'A':0.0,'B':0.0,'C':0.0,'D':0.0,'E':0.0}
+    E = []
+    episode = 100
+    while episode > 0:
+        e = {}
+        for s in V.keys():
+            e[s] = 0.0
+        S = R.start_new_episode()        
+        while True:
+            reward,S_next,is_episode_finish = R.one_step()
+            if is_episode_finish:                
+                error = reward - V[S]
+            else:
+                error = (reward + discount * V[S_next]) - V[S]
+            e[S] = e[S] + 1            
+            for s in V.keys():
+                V[s] = V[s] + alpha * error * e[s]
+                e[s] = discount * lamda * e[s]                 
+            S = S_next
+            if is_episode_finish:       
+                break                   
+        E.append(get_mse(V,R.v))            
+        episode -= 1
+        
+    return E,V
+
+                
+        
+
+def MC(alpha = 0.01):
+    R = RWalk()    
+    V = {'A':0.0,'B':0.0,'C':0.0,'D':0.0,'E':0.0}    
+    E = []    
+    episode = 1000
+    while episode > 0:
+        G = {}
+        was_visited_state = set()
+        S = R.start_new_episode()
+        is_episode_finish = False        
+        while True:
+            was_visited_state.add(S)
+            reward,S_next,is_episode_finish = R.one_step()
+            for s in was_visited_state:
+                if G.has_key(s):
+                    G[s] += reward
+                else:
+                    G[s] = reward
+            if is_episode_finish:
+                break
+            S = S_next
+        
+        for s in was_visited_state:
+            V[s] = V[s] + alpha * (G[s] - V[s])
+        E.append(get_mse(V,R.v))
+        episode -= 1
+    
+    return E,V
+    
     
     
     
