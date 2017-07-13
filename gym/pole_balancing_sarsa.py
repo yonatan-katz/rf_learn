e09e59827249df8ada9e834ebbd9992da658e827
Yonatan <yonic@mail.com>
2017-07-13 21:30:42 +0300

    snapshot

--- gym/pole_balancing_sarsa.py
+++ gym/pole_balancing_sarsa.py
@@ -17,17 +17,18 @@ import pandas as pd
 import cPickle as pickle
 import gym
 
-ACTION_NUM = 3
-VEL_LOW = -0.7
-VEL_HIGH = 0.7
-POS_LOW = -1.2
-POS_HIGH = 0.6
+XMAX = 6
+YMAX = 6
+ZMAX = 6
+KMAX = 6
+DISCRETE_OBSERVATION_SPACE_MAX = XMAX*YMAX*ZMAX*KMAX
+ACTION_NUM = 2
 ACTIONS = range(ACTION_NUM)
 
 def get_best_action(Q,observation_discrete):
     return np.argmax(Q.ix[observation_discrete,:])
     
-def choice_gready(Q,observation_discrete,e=0.3):
+def choice_gready(Q,observation_discrete,e=0.01):
     p = np.random.uniform()
     if p < e:
         return np.random.choice(ACTIONS,1)[0]
@@ -54,36 +55,25 @@ def convert_to_discrete_state(observation,discrete_space):
         d.append(np.digitize([observation[i]],discrete_space[i])[0] - 1)
     return d
 
-def convert_to_flat(discrete_state):
-    return hash(discrete_state) % 10000
+def convert_to_flat(discrete_observation):
+    x,y,z,k = discrete_observation
+    
+    return x+y*XMAX+z*YMAX+k*ZMAX
     
 
 class mc_sarsa:
     def __init__(self):
         self.env = gym.make('CartPole-v0')
-        self.num_of_bins = 10
-        self.pos_space = np.linspace(POS_LOW,POS_HIGH,self.num_of_bins)
-        self.vel_space = np.linspace(VEL_LOW,VEL_HIGH,self.num_of_bins)
-        self.num_of_pos_bin = len(self.pos_space) + 1
-        self.num_of_vel_bin = len(self.vel_space) + 1
+        self.discrete_space = make_discrete_space(self.env.observation_space)
         self.Q = pd.DataFrame(np.zeros((\
-                self.num_of_pos_bin *  self.num_of_vel_bin,\
+                DISCRETE_OBSERVATION_SPACE_MAX,\
                 ACTION_NUM)),\
                 columns=ACTIONS)
         self.df = pd.DataFrame(columns=['episods'])
         self.episod = 0
         self.iteration = 0
-    '''Bining and flaten observation state
-    '''
-    def convert_to_discrete_state(self,observation):
-        pos,vel = observation
-        pos_d = np.digitize([pos],self.pos_space)[0]-1
-        pos_vel = np.digitize([vel],self.vel_space)[0]-1
-        
-        return pos_d + pos_vel * self.num_of_pos_bin
-        
-        
-     
+
+
     def save_q(self,fname):
         with open(fname,'wb') as fd:
             pickle.dump(self.Q,fd)
@@ -92,15 +82,16 @@ class mc_sarsa:
     def sarsa(self,alpha,lamda,episod_num):
         while self.episod < episod_num:
             observation = self.env.reset()
-            s_discrete = self.convert_to_discrete_state(observation)
+            s_discrete = convert_to_flat(convert_to_discrete_state(observation,self.discrete_space))
             a = choice_gready(self.Q,s_discrete)
             E = pd.DataFrame(np.zeros((\
-                self.num_of_pos_bin *  self.num_of_vel_bin,\
+                DISCRETE_OBSERVATION_SPACE_MAX,\
                 ACTION_NUM)),\
                 columns=ACTIONS)            
             while True:
                 observation_prime, r, done, info = self.env.step(a)
-                s_prime_discrete = self.convert_to_discrete_state(observation_prime)
+                self.env.render()
+                s_prime_discrete = convert_to_flat(convert_to_discrete_state(observation_prime,self.discrete_space))
                 a_prime  = choice_gready(self.Q,s_prime_discrete)
                 delta = r + self.Q.ix[s_prime_discrete,a_prime] - \
                     self.Q.ix[s_discrete,a]
@@ -113,7 +104,7 @@ class mc_sarsa:
                 a = a_prime
                 self.iteration += 1
                 self.df.loc[self.iteration] = [self.episod]
-                if r == 0:
+                if done:
                     print "Episode %d is finishing in iteration: %d" % (self.episod,self.iteration)
                     break
                
@@ -123,5 +114,5 @@ class mc_sarsa:
 def main():
     g = mc_sarsa()
     df = g.sarsa(alpha=0.1,lamda=0.5,episod_num=1)
-    g.save_q(fname='C:\\Users\\yonic\\projects\\mountain_car\\q.bin')
+    #g.save_q(fname='C:\\Users\\yonic\\projects\\mountain_car\\q.bin')
     return g
